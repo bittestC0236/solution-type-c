ed4efaea3ecaba895a9ed87368431696c9e747d2
BIT <BIT@DESKTOP-ASOTOMH>
2019-02-28 13:02:29 +0900

    first_commit

--- src/main/java/problem02/MyService.java
+++ src/main/java/problem02/MyService.java
@@ -3,6 +3,24 @@ package problem02;
 public class MyService extends BaseService {
 
 	public String afternoon() {
-		return null;
+		return "오후";
+	}
+	
+	@Override
+	public void service(String state) {
+		String result = "";
+		switch(state) {
+		case "낮":
+			result = super.day();
+			break;
+		case "오후":
+			result = afternoon();
+			break;
+		case "밤":
+			result = super.night();
+			break;
+		}
+		result += "서비스 시작";
+		System.out.println(result);
 	}
 }
